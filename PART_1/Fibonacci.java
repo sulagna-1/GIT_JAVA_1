fb27daeb94418b239a2cd1ddcb0bbb3c867cc6ea
Sulagna Patra <sulagnapatra8@gmail.com>
2025-06-22 02:26:38 +0530

    Create Fibonacci.java

---
+++ PART_1/Fibonacci.java
@@ -0,0 +1,20 @@
+package PART_1;
+
+import java.util.*;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of terms required = ");
+        int n = sc.nextInt();
+        int first = 0;
+        int second = 1;
+        System.out.print("Fibonacci seqjence = " + first + " " + second);
+        for (int i = 2; i < n; i++) {
+            int third = first + second;
+            System.out.print(" " + third);
+            first = second;
+            second = third;
+        }
+    }
+}
